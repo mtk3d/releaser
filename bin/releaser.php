c2af6e689dff2689d09e6b65f1a3880641fd28a1
Mateusz Cholewka <mateusz@cholewka.com.pl>
2020-09-18 12:04:26 +0200

    fix releaser autoload including

--- bin/releaser.php
+++ bin/releaser.php
@@ -1,4 +1,12 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+    require __DIR__ . '/../../../autoload.php';
+} else {
+    fwrite(STDERR, 'ERROR: Composer dependencies not properly set up! Run "composer install" or see README.md for more details' . PHP_EOL);
+    exit(1);
+}
+
 require __DIR__ . '/../src/main.php';
