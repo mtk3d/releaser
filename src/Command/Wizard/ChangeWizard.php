33bafee16dbc76e618ffe52730f8bf286aa585a2
Mateusz Cholewka <mateusz@cholewka.com.pl>
2022-03-06 20:00:46 +0100

    implement change wizard

--- src/Command/Wizard/ChangeWizard.php
+++ src/Command/Wizard/ChangeWizard.php
@@ -2,9 +2,41 @@
 
 declare(strict_types=1);
 
-namespace MTK\Releaser\Command\Wizzard;
+namespace MTK\Releaser\Command\Wizard;
 
-class ChangeWizzard
+use Exception;
+use MTK\Releaser\Change\ChangeType;
+use MTK\Releaser\Command\Change\AuthorProvider;
+use MTK\Releaser\Shared\ChangeDTO;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ChangeWizard
 {
+    private AuthorProvider $authorProvider;
 
+    public function __construct(AuthorProvider $authorProvider)
+    {
+        $this->authorProvider = $authorProvider;
+    }
+
+    public function run(SymfonyStyle $io): ChangeDTO
+    {
+        $type = $io->ask(
+            "What's the type of your change? [fix|feature|deprecation|security|performance|other]",
+            null,
+            function (string $value) {
+                if (!ChangeType::isValid($value)) {
+                    throw new Exception('Change type is invalid');
+                }
+                return $value;
+            }
+        );
+        $message = $io->ask("What is your change about?");
+        $changeId = $io->ask("What's the ID of the change? Ex. Jira ID");
+        $author = $io->ask("Who made the change? Leave empty to get from git config");
+
+        $author = $author ?: $this->authorProvider->getAuthor();
+
+        return new ChangeDTO($type, $message, $author, $changeId);
+    }
 }
