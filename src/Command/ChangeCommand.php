b5f4f7beecda33823aec7643f12e35671501d0ff
Mateusz Cholewka <mateusz@cholewka.com.pl>
2022-03-06 20:01:04 +0100

    use the change wizard inside change command

--- src/Command/ChangeCommand.php
+++ src/Command/ChangeCommand.php
@@ -6,38 +6,48 @@ namespace MTK\Releaser\Command;
 
 use MTK\Releaser\Change\ChangeFacade;
 use MTK\Releaser\Command\Change\AuthorProvider;
+use MTK\Releaser\Command\Wizard\ChangeWizard;
 use MTK\Releaser\Shared\ChangeDTO;
 use Silly\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class ChangeCommand extends Command
 {
     private ChangeFacade $changeFacade;
     private AuthorProvider $authorProvider;
+    private ChangeWizard $changeWizard;
 
     public function __construct(
         ChangeFacade $changeFacade,
-        AuthorProvider $authorProvider
+        AuthorProvider $authorProvider,
+        ChangeWizard $changeWizard
     ) {
         parent::__construct('change');
         $this->changeFacade = $changeFacade;
         $this->authorProvider = $authorProvider;
+        $this->changeWizard = $changeWizard;
     }
 
     public function __invoke(
-        string $type,
-        string $message,
+        ?string $type,
+        ?string $message,
         ?string $changeId,
         ?string $author,
+        InputInterface $input,
         OutputInterface $output
     ): int {
-        $changeId = $changeId ?: "";
+        if (empty($type) || empty($message)) {
+            $changeDto = $this->changeWizard->run(new SymfonyStyle($input, $output));
+        } else {
+            $changeId = $changeId ?: "";
+            $author = $author ?: $this->authorProvider->getAuthor();
 
-        if ($author === null) {
-            $author = $this->authorProvider->getAuthor();
+            $changeDto = new ChangeDTO($type, $message, $author, $changeId);
         }
 
-        $this->changeFacade->create(new ChangeDTO($type, $message, $author, $changeId));
+        $this->changeFacade->create($changeDto);
 
         $output->writeln("Changes to release:");
         $this->changeFacade->getAllChanges()->forEach(function (ChangeDTO $change) use ($output): void {
