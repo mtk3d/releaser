3ef470a6857b0dd13b64bc3b8e0041d04285eedf
Mateusz Cholewka <mateusz@cholewka.com.pl>
2022-03-06 21:58:38 +0100

    implement release wizard questions

--- src/Command/Wizard/ReleaseWizard.php
+++ src/Command/Wizard/ReleaseWizard.php
@@ -4,7 +4,28 @@ declare(strict_types=1);
 
 namespace MTK\Releaser\Command\Wizard;
 
+use Symfony\Component\Console\Style\SymfonyStyle;
+
 class ReleaseWizard
 {
+    /**
+     * @return array{string|null, string|null}
+     */
+    public function run(SymfonyStyle $io): array
+    {
+        $semverPart = $io->choice(
+            "Which part of semver would you to update?",
+            ['major', 'minor', 'patch']
+        );
 
+        $wantStartVersion = $io->confirm("Would you like to specify the start from version?", false);
+
+        $version = null;
+
+        if ($wantStartVersion) {
+            $version = $io->ask("Please specify the start version in semver format");
+        }
+
+        return [$version, $semverPart];
+    }
 }
