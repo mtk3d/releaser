f692ca29ac98d801cda52e10ceb80c2451b7b761
Mateusz Cholewka <mateusz@cholewka.com.pl>
2022-03-06 21:58:56 +0100

    add release wizard and use new arguments names

--- src/Command/ReleaseCommand.php
+++ src/Command/ReleaseCommand.php
@@ -6,34 +6,47 @@ namespace MTK\Releaser\Command;
 
 use MTK\Releaser\Command\Release\PrepareContext;
 use MTK\Releaser\Command\Release\PublishContext;
+use MTK\Releaser\Command\Wizard\ReleaseWizard;
 use PHLAK\SemVer\Exceptions\InvalidVersionException;
 use Silly\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class ReleaseCommand extends Command
 {
     private PrepareContext $prepareContext;
     private PublishContext $publishContext;
+    private ReleaseWizard $releaseWizard;
 
-    public function __construct(PrepareContext $prepareContext, PublishContext $publishContext)
-    {
+    public function __construct(
+        PrepareContext $prepareContext,
+        PublishContext $publishContext,
+        ReleaseWizard $releaseWizard
+    ) {
         parent::__construct('release');
         $this->prepareContext = $prepareContext;
         $this->publishContext = $publishContext;
+        $this->releaseWizard = $releaseWizard;
     }
 
     public function __invoke(
-        ?string $ver,
-        ?string $type,
+        ?string $startVersion,
+        ?string $semverPart,
+        InputInterface $input,
         OutputInterface $output
     ): int {
+        if (empty($semverPart)) {
+            list($startVersion, $semverPart) = $this->releaseWizard->run(new SymfonyStyle($input, $output));
+        }
+
         if ($this->publishContext->hasUncommittedChanges()) {
             $output->write("You have uncommitted changes in your repository", true);
             return 1;
         }
 
         try {
-            $release = $this->prepareContext->prepareRelease($type, $ver);
+            $release = $this->prepareContext->prepareRelease($semverPart, $startVersion);
         } catch (InvalidVersionException $e) {
             $output->write("Version format error", true);
             return 1;
